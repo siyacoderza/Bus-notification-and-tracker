061fe1349d4127af65ae7339f3af857df367d5ad
Siyabonga Ngcobo <32388934+siyacoderza@users.noreply.github.com>
2026-03-02 12:33:09 +0200

    Refactor drizzle.config to simplify configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,19 +1,9 @@
 import 'dotenv/config';
 
-import { defineConfig } from "drizzle-kit";
-
-if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL, ensure the database is provisioned");
-}
-
-export default defineConfig({
-  out: "./drizzle",
-  schema: "./db/schema.ts",
-  dialect: "postgresql",
+export default {
+  schema: './db/schema.ts',
+  out: './drizzle',
   dbCredentials: {
-    url: process.env.DATABASE_URL ||
-   process.env.NETLIFY_DATABASE_URL,
+    url: process.env.DATABASE_URL || process.env.NETLIFY_DATABASE_URL,
   },
-  
-  },
-});
+};
